abd64c138d10c5c677e8f905b1a5d1683a98ef1b
Rafael <rafaelmoreiracvo@yahoo.com.br>
2019-12-11 17:42:24 -0300

    curso finalizando

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
 
 from django.http import HttpResponse
 import datetime
 
+
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['now'] = datetime.datetime.now()
+    data['transacoes'] = ['t1', 't2', 't3']
     # html = "<html><body>Hello Word! Esta é a hora atual %s.</body></html>" % now
     # return HttpResponse(html)
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+    data = {}
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+
+    return render(request, 'contas/form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
